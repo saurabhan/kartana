be77d2ae7f8a1f3beeb2215b4aa87550c18abb5d
saurabhan <saurabhan.001@gmail.com>
2022-06-27 13:20:27 +0530

    CSS fix and style changes

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import Task from "./Task";
 import { HiPlus } from "react-icons/hi";
+import {TbBrightness, TbCircleDotted } from "react-icons/tb";
+import { BsCheckCircleFill, BsCircle, BsThreeDotsVertical } from "react-icons/bs";
 
 interface Props {
   item: {
@@ -21,14 +23,27 @@ const Column = (props: Props) => {
   const { item, index } = props;
   return (
     <>
-      <div className="flex-1 p-5 bg-gray-100 rounded-md">
-        <div className="flex items-center justify-between">
-        <h1 className="font-bold text-lg">{item.status} {" "} {item.count}</h1>
+      <div className="flex-1 relative bg-gray-100 rounded-md overflow-hidden overflow-y-scroll scrollbar-hide">
+        <div className="sticky top-0 p-2  z-10 w-full flex border-b-2 border-spacing-2 items-center justify-between bg-gray-200">
+          <div className="flex gap-2 items-center">
+          {
+            item.status === "backlog" ? <TbCircleDotted className=" w-5 h-5" /> :
+            item.status === "in-progress" ? <TbBrightness className=" w-5 h-5" /> :
+            item.status === "done" ? <BsCheckCircleFill className="text-blue-500 w-5 h-5" /> :
+            item.status === "todo" ? <BsCircle className=" w-4 h-4" /> :
+            ""
+          }
+        <p className="font-bold text-lg">{item.status} {" "} <span className="font-normal">{item.count}</span></p>
+          </div>
+          <div className="flex gap-2">
+
         <HiPlus/>
+        <BsThreeDotsVertical/>
+          </div>
         </div>
         <Droppable droppableId={item.status}>
           {(provided, snapshot) => (
-            <ul ref={provided.innerRef} {...provided.droppableProps}>
+            <ul ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2 mt-2 ml-2 mr-2">
               {item.tasks.length > 0 ? (
                 item.tasks?.map((task, index) => {
                   return (
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsCircleFill } from "react-icons/bs";
 import { Draggable } from "react-beautiful-dnd";
+import {MdSignalCellular1Bar, MdSignalCellular2Bar, MdSignalCellular3Bar} from "react-icons/md";
+import Image from "next/image";
 interface Props {
   task: {
     id: string;
@@ -11,7 +13,7 @@ interface Props {
   index: number;
 }
 
-const priorityColors = {
+const priorityIcons = {
   low: "text-green-200",
   medium: "text-yellow-200",
   high: "text-red-200",
@@ -33,19 +35,29 @@ const Task = (props: Props) => {
             ref={provided.innerRef}
           >
             <div
-              className={`  relative  bg-white m-2 p-5 rounded-lg shadow-lg`}
+              className={`  relative flex flex-col gap-2  bg-white p-5 rounded-lg shadow-lg`}
             >
-              <BsCircleFill
-                className={`w-3 h-3 right-2 top-2 absolute ${
-                  priorityColors[task?.priority as keyof typeof priorityColors]
-                }`}
-              />
-              <p className="font-semibold">{task?.name}</p>
-              {task?.assignedTo && (
-                <div className="pt-5 text-sm ">
-                  assigned to : {task?.assignedTo?.name}
+              <div className="flex justify-between">
+
+              <p className="text-sm ">{task?.id}</p>
+              {task?.assignedTo ? (
+                <div className="">
+                  <Image className="rounded-full" src={`https://avatars.dicebear.com/api/initials/${task?.assignedTo?.name}.svg`} width={20} height={20} />
                 </div>
+              ) : (
+                <div className="">
+                  <Image className="rounded-full" src={`https://avatars.dicebear.com/api/identicon/unkown.svg`} width={20} height={20} />
+                  </div>
               )}
+              </div>
+              <p className="font-semibold">{task?.name}</p>
+              {
+                task.priority && (
+                  task?.priority === "low" ? <MdSignalCellular1Bar/> :
+                  task?.priority === "medium" ? <MdSignalCellular2Bar/> :
+                  task?.priority === "high" ? <MdSignalCellular3Bar/> :
+                  "")
+              }
             </div>
           </div>
         )}
